2daf3cda378e4371e568c3e54ed9387743335a4b
NanaChan <pathpeemeil@gmail.com>
2022-12-24 03:24:04 +0700

    Level 2

---
+++ lib/home/IndexPreview.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:molten_navigationbar_flutter/molten_navigationbar_flutter.dart';
+
+class IndexPreview extends StatefulWidget {
+  const IndexPreview({Key? key}) : super(key: key);
+
+  @override
+  State<IndexPreview> createState() => _IndexPreview();
+}
+
+class _IndexPreview extends State<IndexPreview> with TickerProviderStateMixin{
+  final List<String> StylesList = [
+    'Minimal Style','Sweet Style','Vintage Style','Sexy Style','Street Style','Casual Style'
+  ];
+  int _currentIndex = 0;
+  int _activeIndex = 0;
+
+  List<String> pageList = ['Minimal Style','Sweet Style','Vintage Style','Sexy Style','Street Style','Casual Style'
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        toolbarHeight: 100,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/LOGO1.png',
+              fit: BoxFit.contain,
+              height: 80,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: MoltenBottomNavigationBar(
+          selectedIndex: _currentIndex,
+          onTabChange: (clickedIndex) {
+            setState(() {
+              _currentIndex = clickedIndex;
+            });
+          },
+          margin: EdgeInsets.all(20),
+          borderRaduis: BorderRadius.circular(30),
+          domeCircleSize: 50,
+          domeCircleColor: Color.fromRGBO(222, 179, 173, 1.0),
+          barColor: Color.fromRGBO(223, 211, 248, 1.0),
+          tabs: [
+            MoltenTab(
+              icon: Icon(Icons.home, size: 30),
+            ),
+            MoltenTab(
+              icon: Icon(Icons.camera_alt, size: 30),
+            ),
+            MoltenTab(
+              icon: Icon(Icons.person, size: 30),
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 85,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    buildButton(),
+                  ],
+                ),
+              ),
+              Container(
+                child: Text(
+                    pageList[_activeIndex]
+                ),
+              ),
+              //_buildSubPage(),
+            ],
+          ),
+        ),
+      ),
+    );
+
+  }
+  Widget buildButton() {
+    return Row(
+      children: [
+        for (int i = 0; i < StylesList.length; i++)
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _activeIndex = i;
+                  });
+                },
+                child: Text(
+                  '${StylesList[i]}',
+                  style: TextStyle(
+                      fontSize: 16
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(shape: StadiumBorder(),
+                    primary: (_activeIndex==i)?Color.fromRGBO(222, 179, 173, 1.0):Color.fromRGBO(223, 211, 248, 1.0))
+            ),
+          ),
+      ],
+    );
+  }
+  /*Widget _buildSubPage(){
+    switch (_currentIndex){
+      case 0 : return HomePage();
+      case 1 : return MainCamera();
+      case 2 : return ProfilePage();
+      default: return SizedBox.shrink();
+    }
+  }*/
+}
---
+++ lib/home/StartPage.dart
@@ -0,0 +1,56 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:style_app/home/IndexPreview.dart';
+
+class StartPage extends StatefulWidget {
+  const StartPage({Key? key}) : super(key: key);
+
+  @override
+  _StartPageState createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color.fromRGBO(222, 179, 173, 1.0),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 150),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/LOGO1.png',
+              fit: BoxFit.contain,
+              height: 250,
+              width: 250,
+              color: Colors.white,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 200),
+              child: ElevatedButton(
+                onPressed: (){
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (BuildContext context){
+                        return const IndexPreview();
+                      }
+                      )
+                  );
+                },
+                child: Text(
+                  'GET START',
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: 'assets/fonts/Inter-VariableFont_slnt,wght.ttf'
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(shape: StadiumBorder(),primary: Colors.brown),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
